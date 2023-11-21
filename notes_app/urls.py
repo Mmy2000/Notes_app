1bf4539663bcb3d01952d21c8e081b53f34d18eb
Mmy2000 <yahianada005@gmail.com>
2023-11-21 23:45:05 +0200

    fixing some issues

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -5,7 +5,7 @@ app_name = 'notes_app'
 
 urlpatterns = [
     path('' , views.notelist , name='post_list'),
-    path('<int:id>/' , views.post_details , name='post_detail'),
+    path('id=<int:id>/' , views.post_details , name='post_detail'),
     path('tags/<str:slug>' , views.PostByTags.as_view() , name='post_by_tags'),
     path('add/' ,views.note_add , name='add_note'),
     path('<int:id>/edit' , views.edit,name='note_edit'),
